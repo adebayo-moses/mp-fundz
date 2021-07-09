b39e55a1862777d75dcd09b6e1ec93f3735e9b59
kunleadeoye <zenesoftinc@gmail.com>
2021-07-09 16:30:49 -0700

    winners

--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,8 @@ Route::get('get_winners', function() {
 
         $users = DB::table('entries')
              ->select(DB::raw('user_id, sum(entry)'))
-             ->groupBy('user_id')
-             ->orderBy('entry', 'DESC')->get();
+             ->groupBy('user_id');
+            //  ->orderBy('entry', 'DESC')->get();
 
              dd($users);
     }else {
